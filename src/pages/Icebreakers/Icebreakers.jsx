3621c96fa28c217ce0afacb21698c4badb026c95
Haziq Naeem <haziqnaeem36@gmail.com>
2022-03-31 21:36:38 -0400

    start style

--- src/pages/Icebreakers/Icebreakers.jsx
+++ src/pages/Icebreakers/Icebreakers.jsx
@@ -1,55 +1,56 @@
 import { Link } from "react-router-dom";
-
-import "./icebreaker.scss"
-
-import React from 'react'
-
+import "./icebreaker.scss";
+import React from "react";
 
 const Icebreakers = (props) => {
-
   const IceBreakerList = props.iceBreakers.map((icebreaker) => {
-    console.log(icebreaker)
-    
-    const profId = icebreaker.owner._id
+    console.log(icebreaker);
+
+    const profId = icebreaker.owner._id;
     return (
-
-      <>
-      <div className="container">
-
       <React.Fragment key={icebreaker._id}>
-      <div>
-        <Link to={`/profiles/${profId}`}>
-
-        <img src={icebreaker.owner.photo ? icebreaker.owner.photo : 'No Image'} alt={'Person'} />
-        <h2 key={profId} className="name">{icebreaker.owner.name}</h2>
-        </Link>
-        <div className="info">
-        <p>Fun fact: {icebreaker.funFact}</p>
-        <p>Question: {icebreaker.question}</p>
+        <div className="container">
+          <div>
+            <Link to={`/profiles/${profId}`}>
+              <img
+                src={
+                  icebreaker.owner.photo ? icebreaker.owner.photo : "No Image"
+                }
+                alt={"Person"}
+              />
+              <h2 key={profId} className="name">
+                {icebreaker.owner.name}
+              </h2>
+            </Link>
+            <div className="info">
+              <p>Fun fact: {icebreaker.funFact}</p>
+              <p>Question: {icebreaker.question}</p>
+            </div>
+            {icebreaker.owner.email === props.user.email ? (
+              <button
+                onClick={() => props.handleDeleteIceBreaker(icebreaker._id)}
+                className="dlt-btn"
+              >
+                X
+              </button>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
-        {icebreaker.owner.email === props.user.email ?
-        <button onClick={() => props.handleDeleteIceBreaker(icebreaker._id)} className="dlt-btn">
-          X
-        </button>
-        : ''
-        }
-      <div>
-      </div>
-      </div>
       </React.Fragment>
     );
   });
 
-
   return (
     <div className="mainbody">
       <h1 className="title">All Icebreakers</h1>
-      <Link to="/addicebreaker" >
+      <Link to="/addicebreaker">
         <button className="add-IB">Add Ice Breaker</button>
       </Link>
       <div>{IceBreakerList}</div>
     </div>
   );
-}
- 
+};
+
 export default Icebreakers;
